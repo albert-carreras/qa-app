c0a24f107c141c1c5dd5fd44501062016c458d63
Albert Carreras <albert.carreras@mollie.com>
2020-02-23 01:30:57 +0100

    Question list + New question containers

---
+++ src/containers/NewQuestion/index.tsx
@@ -0,0 +1,142 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Formik, Form } from "formik";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+import Heading from "components/Heading";
+import { questionActions } from "state/question";
+
+import "./styles.scss";
+
+// Types
+import { Dispatch } from "redux";
+import { CreateQuestion } from "state/question/types";
+import { Question as QuestionType } from "types/common.d";
+
+interface Props {
+  createQuestion: Function;
+}
+
+/**
+ * This container uses Formik to handle the new question form.
+ * I also added the Delay checkbox.
+ */
+
+const NewQuestion = ({ createQuestion }: Props) => {
+  return (
+    <div>
+      <Heading tooltipText='Here you can find the created questions and their answers'>
+        Create a new question
+      </Heading>
+      <div className='form'>
+        <Formik
+          // Sets up our default values
+          initialValues={{ question: "", answer: "", delay: false }}
+          // Validates our data
+          validate={values => {
+            const errors: { question?: string; answer?: string } = {};
+
+            if (!values.question) {
+              errors.question = "Required field";
+            } else if (values.question.length < 3) {
+              errors.question =
+                "The question should be longer than 3 characters";
+            }
+
+            if (!values.answer) errors.answer = "Required field";
+
+            return errors;
+          }}
+          // Handles our submission
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            if (values.delay) {
+              setTimeout(() => {
+                // Remove delay from request
+                const { delay, ...question } = values;
+                createQuestion(question);
+                setSubmitting(false);
+              }, 5000);
+            } else {
+              createQuestion(values);
+              setSubmitting(false);
+              resetForm();
+            }
+          }}
+        >
+          {props => (
+            <Form>
+              <div className='new-question__field'>
+                <TextField
+                  variant='outlined'
+                  label='Question'
+                  name='question'
+                  fullWidth
+                  margin='normal'
+                  placeholder='Write your question here'
+                  value={props.values.question}
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                  helperText={
+                    props.errors.question ? props.errors.question : ""
+                  }
+                  error={
+                    props.touched.question && Boolean(props.errors.question)
+                  }
+                />
+              </div>
+              <div className='new-question__field'>
+                <TextField
+                  variant='outlined'
+                  label='Answer'
+                  fullWidth
+                  name='answer'
+                  rows='3'
+                  multiline
+                  margin='normal'
+                  helperText={props.errors.answer ? props.errors.answer : ""}
+                  error={props.touched.answer && Boolean(props.errors.answer)}
+                  placeholder='Write your answer here'
+                  value={props.values.answer}
+                  onChange={props.handleChange}
+                  onBlur={props.handleBlur}
+                />
+              </div>
+              <div className='new-question__submit-button-group'>
+                <Button
+                  type='submit'
+                  color='primary'
+                  variant='contained'
+                  disabled={props.isSubmitting}
+                >
+                  Create Question
+                </Button>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name='delay'
+                      value='delay'
+                      onChange={props.handleChange}
+                      checked={props.values.delay}
+                    />
+                  }
+                  label='Add Delay'
+                />
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<CreateQuestion>) => ({
+  createQuestion: (question: QuestionType) =>
+    dispatch(questionActions.createQuestion(question))
+});
+
+export default connect(null, mapDispatchToProps)(NewQuestion);
--- src/containers/questionList/index.tsx
+++ src/containers/questionList/index.tsx
@@ -1,32 +1,117 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+import Heading from "components/Heading";
+import Question from "components/Question";
 
 import { questionActions, questionSelectors } from "state/question";
 
-const QuestionList = ({ questions, readQuestions }) => {
+import "./styles.scss";
+
+// Types
+import { Dispatch } from "redux";
+import { Question as QuestionType } from "types/common.d";
+import { State } from "types/state.d";
+import {
+  SortQuestions,
+  ReadQuestions,
+  UpdateQuestion,
+  DeleteQuestion
+} from "state/question/types";
+interface Props {
+  questions: Array<QuestionType>;
+  readQuestions: Function;
+  deleteQuestion: Function;
+  updateQuestion: Function;
+  sortQuestions: Function;
+}
+
+const QuestionList = ({
+  questions,
+  readQuestions,
+  deleteQuestion,
+  updateQuestion,
+  sortQuestions
+}: Props) => {
   useEffect(() => {
     readQuestions();
-  });
-
+  }, [readQuestions]);
+  const handleDeleteQuestion = (id: Number) => {
+    deleteQuestion({ questionIds: [id], all: false });
+  };
+  const handleupdateQuestion = (question: QuestionType) => {
+    updateQuestion(question);
+  };
   return (
     <>
-      {questions &&
-        questions.map((q, i) => (
-          <>
-            <p key={`question-${i}`}>ID: {q.id}</p>
-            <p key={`question-${i}`}>Q: {q.question}</p>
-            <p key={`answer-${i}`}>A: {q.answer}</p>
-          </>
-        ))}
+      <Heading tooltipText='Here you can create new questions and their answers'>
+        Created Questions
+      </Heading>
+      {questions && questions.length > 0 ? (
+        questions.map((question, i) => (
+          <Question
+            key={`question-${i}`}
+            question={question}
+            deleteQuestion={handleDeleteQuestion}
+            updateQuestion={handleupdateQuestion}
+          />
+        ))
+      ) : (
+        <Paper
+          variant='outlined'
+          elevation={3}
+          style={{
+            backgroundColor: "rgba(200,0,0,0.25)",
+            padding: 20,
+            color: "rgb(200,0,0)"
+          }}
+        >
+          No questions yet :-(
+        </Paper>
+      )}
+      <br />
+      <ButtonGroup variant='contained'>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => sortQuestions()}
+        >
+          Sort questions
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          onClick={() => deleteQuestion({ all: true })}
+        >
+          Delete ALL questions
+        </Button>
+      </ButtonGroup>
     </>
   );
 };
 
-const mapDispatchToProps = dispatch => ({
-  readQuestions: () => dispatch(questionActions.readQuestions())
+const mapDispatchToProps = (
+  dispatch: Dispatch<
+    SortQuestions | ReadQuestions | UpdateQuestion | DeleteQuestion
+  >
+) => ({
+  sortQuestions: () => dispatch(questionActions.sortQuestions()),
+  readQuestions: () => dispatch(questionActions.readQuestions()),
+  updateQuestion: (question: QuestionType) =>
+    dispatch(questionActions.updateQuestion(question)),
+  deleteQuestion: ({
+    questionIds,
+    all
+  }: {
+    questionIds: Array<number> & never[];
+    all: boolean;
+  }) => dispatch(questionActions.deleteQuestion({ questionIds, all }))
 });
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: State) => ({
   questions: questionSelectors.getQuestions(state)
 });
 export default connect(mapStateToProps, mapDispatchToProps)(QuestionList);
