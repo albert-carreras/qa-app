b598b2213fd62d66ef99f572bf8ff4e08364f09b
Albert Carreras <albert.carreras@mollie.com>
2020-02-23 01:15:44 +0100

    Main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,23 @@
 import React from "react";
-import QuestionList from "containers/questionList";
+import QuestionList from "containers/QuestionList";
+import NewQuestion from "containers/NewQuestion";
+import Sidebar from "containers/Sidebar";
+
+import "./styles.scss";
 
 const Main = () => (
   <>
-    <QuestionList />
+    <h1>Very cool Q&A App</h1>
+    <div className='main__container'>
+      <div className="main__left">
+        <Sidebar />
+      </div>
+      <div className="main__right">
+        <QuestionList />
+        <hr />
+        <NewQuestion />
+      </div>
+    </div>
   </>
 );
 
